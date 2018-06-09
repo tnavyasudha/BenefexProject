3a664a03e91ee287165cbfdde175d6060db5a85d
Navya Tippanaboyana <Navya.Tippanaboyana@arm.com>
2018-06-09 23:20:22 +0100

    tests to add amount to basket, check warning message if its greater tham max amount

--- src/main/java/com/benefex/childcarebenefits/pages/ChildcareVouchersPage.java
+++ src/main/java/com/benefex/childcarebenefits/pages/ChildcareVouchersPage.java
@@ -1,17 +1,36 @@
 package com.benefex.childcarebenefits.pages;
 
+import java.util.Random;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class ChildcareVouchersPage {
 	
-	@FindBy(how= How.CLASS_NAME , using ="full-tile-link focused")
+	@FindBy(how= How.CSS , using ="a[href*='/benefexdemo/flex/shop/summary/ccvallsave']")
     private WebElement childcareVouchers=null;
 	
 	@FindBy(how= How.XPATH, using =".//*[@id='shop']/h1")
     private WebElement childcareVoucherHeader=null;
 	
+	@FindBy(how= How.XPATH, using =".//*[@id='editBenefit']")
+    private WebElement makeChangesButton =null;
+	
+	@FindBy(how= How.XPATH, using =".//*[@id='shop']/div[1]/div/p/span")
+    private WebElement maxTaxFreeAmount =null;
+	
+	@FindBy(how= How.XPATH, using =".//*[@id='ccvallsave_c_employeemonthlycost']")
+	private WebElement pleaseEnterAmount =null;
+	
+	@FindBy(how= How.XPATH, using =".//*[@id='ccvallsave_c_termsconditions']")
+	private WebElement termsAndConditionsBeforeAddingToBasket =null;
+	
+	@FindBy(how= How.XPATH, using =".//*[@id='AddToBasket']")
+	private WebElement addToBasket =null;
+	
+	@FindBy(how= How.XPATH, using =".//*[@id='exceptions']/div")
+	private WebElement youCannotSelectGreaterThanYourTaxFreeValueText =null;
 	
 	/*
      * Method to click on child care Vouchers
@@ -27,5 +46,59 @@ public class ChildcareVouchersPage {
         return childcareVoucherHeader.getText();
     
     }
+    
+    /*
+     * Method to click on 'Make changes' button
+     */
+    public void clickOnMakeChanges() {
+    	makeChangesButton.click();
+    }
 
+    /*
+     * Method to get max tax free amount of the employee 
+     */
+    public String getmaxtaxFreeAmountOfTheEmployee() {
+        return maxTaxFreeAmount.getText();
+    
+    }
+    
+    /*
+     * Method to enter number in 'please enter an amount' textbox
+     */
+    public void enterAmount(String amount) {
+    	pleaseEnterAmount.sendKeys(amount);
+    }
+    
+    /*
+     * Method to check the terms and conditions check box is checked or not
+     */
+    public boolean termsAndConditionsBeforeAddingToBasket() {
+    	return termsAndConditionsBeforeAddingToBasket.isSelected();
+    }
+
+
+    /*
+     * Method to click on 'Add to basket' button
+     */
+    public void clickOnAddTobasket() {
+    	addToBasket.click();
+    }
+    
+    /*
+     * Method to generate a random number
+     */
+    public int getAmountGreaterThanMaxAmountOfTheEmployee(String amount) {
+    	Random rand = new Random();
+    	int x = rand.nextInt(900) + Integer.valueOf(amount);
+    	return x;
+    }
+    
+    /*
+     * Method to get 'You cannot select greater than your tax free value.' text 
+     */
+    public String getYouCannotSelectGreaterThanYourTaxFreeValue() {
+        return youCannotSelectGreaterThanYourTaxFreeValueText.getText();
+    
+    }
+    
 }
--- src/test/java/com/benefex/childcarebenefits/tests/IncreaseChildCareBenefitsTests.java
+++ src/test/java/com/benefex/childcarebenefits/tests/IncreaseChildCareBenefitsTests.java
@@ -1,19 +1,27 @@
 package com.benefex.childcarebenefits.tests;
 
 import org.testng.annotations.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 import com.benefex.childcarebenefits.utility.WebDriverTestBase;
+import java.util.Random;
 
 import static org.hamcrest.Matchers.is;
 
 public class IncreaseChildCareBenefitsTests extends WebDriverTestBase{
 
 	/**
-     * This test for the authentication required to login into The reward hub website 
-     * @author Navya Sudha Tippanaboyana
+     * This test to
+     * Check the user is an existing employee
+     * Check the user has a child care voucher benefits
+     * Make changes to existing amount
+     * Add changes to basket
+     * Confirm changes in basket 
+     * 
+     * @author Navya Sudha Tippanaboyana 
      */
 	@Test(description = "Login to the reward hub website and")
     public void increaseChildCareBenefitsTests() throws Exception {
+		// Loginto website
 		logIntoTheRewardHub("ProdDemo14" , "longWater+35");
 		// click on benefits 
 		pages.benefexDemoHomePage().clickOnBenefits();
@@ -21,6 +29,55 @@ public class IncreaseChildCareBenefitsTests extends WebDriverTestBase{
 		pages.childcareVouchersPage().clickOnChildCareVouchers();
 		// check the landed page is childcareVoucherHeader
 		helper.verifyThat("Checking the page has a header - Childcare Vouchers", pages.childcareVouchersPage().getChildcareVouchersText() , is("Childcare Vouchers"));
-		// 
+		// click on 'Make Changes' button
+		pages.childcareVouchersPage().clickOnMakeChanges();
+		// get the maximum allowed tax free amount of the employee
+		String amount = pages.childcareVouchersPage().getmaxtaxFreeAmountOfTheEmployee();
+		amount = amount.replace("£", "");
+		System.out.println("Amount == " + amount);
+		// enter amount in the 'Please enter amount' text box
+		pages.childcareVouchersPage().enterAmount(amount);
+		// check that the 'T & C check box is checked
+		assertThat(pages.childcareVouchersPage().termsAndConditionsBeforeAddingToBasket(), is(true));
+		// click on 'Add to basket'
+		pages.childcareVouchersPage().clickOnAddTobasket();
+		
+		
+	}
+	
+	/**
+     * This test to
+     * Check the user is an existing employee
+     * Check the user has a child care voucher benefits
+     * Make changes to existing amount (greater than the max tax free amount of the employee)
+     * check for error message 
+     * 
+     * @author Navya Sudha Tippanaboyana 
+     */
+	@Test(description = "Login to the reward hub website and")
+    public void unableToIncreaseChildCareBenefitsTests() throws Exception {
+		// Loginto website
+		logIntoTheRewardHub("ProdDemo14" , "longWater+35");
+		// click on benefits 
+		pages.benefexDemoHomePage().clickOnBenefits();
+		// click on child care vouchers
+		pages.childcareVouchersPage().clickOnChildCareVouchers();
+		// check the landed page is childcareVoucherHeader
+		helper.verifyThat("Checking the page has a header - Childcare Vouchers", pages.childcareVouchersPage().getChildcareVouchersText() , is("Childcare Vouchers"));
+		// click on 'Make Changes' button
+		pages.childcareVouchersPage().clickOnMakeChanges();
+		// get the maximum allowed tax free amount of the employee
+		String amount = pages.childcareVouchersPage().getmaxtaxFreeAmountOfTheEmployee();
+		amount = amount.replace("£", "");
+		System.out.println("Amount == " + amount);
+		// enter an amount greater than max amount if the employee
+		int amountGreaterThanMaxAmount = pages.childcareVouchersPage().getAmountGreaterThanMaxAmountOfTheEmployee(amount);
+		// enter amount in the 'Please enter amount' text box
+		pages.childcareVouchersPage().enterAmount("" +amountGreaterThanMaxAmount);
+		// check that warning message 'You cannot select greater than your tax free value.' occurs or not
+		helper.verifyThat("Checking the warning message - You cannot select greater than your tax free value.", pages.childcareVouchersPage().getYouCannotSelectGreaterThanYourTaxFreeValue() , is("You cannot select greater than your tax free value."));
+		
+		
+		
 	}
 }
